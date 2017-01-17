a4781b408f1f79c0786c5424ea1c43bc98378799
iadfeshchm <zlatov@ya.ru>
2017-01-17 13:28:03 +0300

    Возможность отключить в настройках проекта

--- Prefixw.py
+++ Prefixw.py
@@ -2,7 +2,12 @@ import sublime, sublime_plugin, pprint, subprocess
 
 class EventListener(sublime_plugin.EventListener):
 	def on_activated_async(self, view):
-		output = subprocess.check_output("~/.config/sublime-text-3/Packages/Prefixw/prefixw.sh", shell=True)
-		output = output.decode("utf-8").rstrip('\n')
-		if output!='done':
-			pprint.pprint(output)
+		projectSettings = self.loadProjectSettings(view)
+		if projectSettings == False or projectSettings["on"] == True:
+			output = subprocess.check_output("~/.config/sublime-text-3/Packages/Prefixw/prefixw.sh", shell=True)
+			output = output.decode("utf-8").rstrip('\n')
+			if output!='done':
+				pprint.pprint(output)
+
+	def loadProjectSettings(self, view):
+		return view.settings().get("prefixw")
