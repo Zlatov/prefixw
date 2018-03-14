3e7e15a00c7708516bf1d27aa67bf93c310f2b09
iadfeshchm <zlatov@ya.ru>
2018-03-14 12:13:06 +0300

    fix start index for cut project name

--- prefixw.sh
+++ prefixw.sh
@@ -6,9 +6,16 @@ if [[ status -eq 0 ]]
         for pid in $pids; do
             name=$(xdotool getwindowname $pid)
             if [[ $name == *" Sublime Text"* ]]; then
+                # Определяем индекс первого вхождения символов „(“ и „)“.
                 start=`expr index "$name" '\('`
                 finish=`expr index "$name" '\)'`
+                # Длинна имени проекта
                 let "len=finish-start-1"
+                # Фиксим начало вырезания если в имени таба есть символ • (не сохранён)
+                IS_FILE_NOT_SAVED=`if [[ "$name" = *"•"* ]]; then echo true; else echo false; fi;`
+                if [[ "$IS_FILE_NOT_SAVED" = true ]]; then
+                    let "start = $start - 2"
+                fi
                 newTitle=${name:start:len}
                 size=${#newTitle}
                 if [[ $size -gt 0 ]]; then
