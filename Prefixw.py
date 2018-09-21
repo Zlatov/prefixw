37ebf9a01415c5b9e3936e67b5520b27e0cf899a
iadfeshchm <zlatov@ya.ru>
2018-09-21 14:08:30 +0300

    add Current Path to Status

--- Prefixw.py
+++ Prefixw.py
@@ -1,5 +1,34 @@
 import sublime, sublime_plugin, pprint, subprocess, re
 from pprint import pprint
+import os
+
+class CurrentPathStatusCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		view = self.view # вьюха открытого файла?
+		window = view.window() # экземпляр окна сублайма?
+		file_path = view.file_name()
+		if file_path == None:
+			return None
+		# Определён полный путь к открытому файлу
+		# pprint(file_path)
+
+		project_path = window.project_file_name()
+		if project_path == None:
+			project_path = window.folders()[0]
+		else:
+			project_path = os.path.dirname(project_path)
+
+		if project_path == None:
+			view.set_status('current_path', file_path)
+			return None
+		project_path_len = len(project_path)
+		# pprint(project_path)
+		# pprint(file_path[:project_path_len])
+		if file_path[:project_path_len] == project_path:
+			current_path = file_path[project_path_len:]
+		else:
+			current_path = file_path
+		view.set_status('current_path', current_path)
 
 class EventListener(sublime_plugin.EventListener):
 	def on_activated_async(self, view):
@@ -7,8 +36,10 @@ class EventListener(sublime_plugin.EventListener):
 		if projectSettings:
 			if projectSettings["on"] == True:
 				self.renameTab()
+				view.run_command('current_path_status')
 		else:
 			self.renameTab()
+			view.run_command('current_path_status')
 
 	def on_activated(self, view):
 		self.renameTab()
